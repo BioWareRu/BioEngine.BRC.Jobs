9b6c2861a40a5286a44da16899e1e2eab9be96f2
George <sonicgd@gmail.com>
2019-10-18 14:58:01 +0500

    Extract users to separate model. Allow to pass user id type so it can be defined on application level, not hardcoded as string. Separate content items. Don't use single table for them.

--- src/BioEngine.BRC.Jobs/JobsModule.cs
+++ src/BioEngine.BRC.Jobs/JobsModule.cs
@@ -1,6 +1,4 @@
-using BioEngine.Core.DB;
 using BioEngine.Core.Modules;
-using BioEngine.Core.Posts.Entities;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,13 +7,8 @@ namespace BioEngine.BRC.Jobs
 {
     public class JobsModule : BaseBioEngineModule
     {
-        public override void ConfigureEntities(IServiceCollection serviceCollection, BioEntitiesManager entitiesManager)
-        {
-            base.ConfigureEntities(serviceCollection, entitiesManager);
-            RegisterRepositories(typeof(Post).Assembly, serviceCollection, entitiesManager);
-        }
-
-        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostEnvironment environment)
         {
             base.ConfigureServices(services, configuration, environment);
             services.AddSingleton<IHostedService, JobsService>();
